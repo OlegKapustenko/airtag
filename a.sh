84b1bf0da845989b7972081e52ad762235f6c7e4
Oleg Kapustenko <o.kapustenko@eisgroup.com>
2024-01-01 15:16:05 +0100

    a.sh new pendals

--- a.sh
+++ a.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 while true; do
-  /Users/oleg/AirTag/dailyGPX.sh "White 1" static
   /Users/oleg/AirTag/dailyGPX.sh "Oleg 1" iTag
   /Users/oleg/AirTag/dailyGPX.sh "Olga 1" iTag
+  /Users/oleg/AirTag/dailyGPX.sh "KR9N176" iTag
   /Users/oleg/AirTag/dailyGPX.sh "Black 1" static
-  /Users/oleg/AirTag/dailyGPX.sh "KR9N176" static
+  /Users/oleg/AirTag/dailyGPX.sh "Valik" denysovy
+  /Users/oleg/AirTag/dailyGPX.sh "Olesia" denysovy
+  /Users/oleg/AirTag/dailyGPX.sh "Natalia" denysovy
   /Users/oleg/AirTag/dailyGPX.sh "Kate round" kate
   /Users/oleg/AirTag/dailyGPX.sh "Kate long" kate
+  /Users/oleg/AirTag/dailyGPX.sh "Alex1" alex
+  /Users/oleg/AirTag/dailyGPX.sh "Alex2" alex
   echo -n "."
   sleep 60
 done
